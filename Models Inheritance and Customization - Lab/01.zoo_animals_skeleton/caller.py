ffdca22d5b4c33963162b7537b469f0c8cfd3a7b
Rumyana Valdobreva <rumyiwanowa@abv.bg>
2024-07-09 23:29:43 +0300

    Adding Models Inheritance and Customization - Lab

---
+++ Models Inheritance and Customization - Lab/01.zoo_animals_skeleton/caller.py
@@ -0,0 +1,11 @@
+import os
+import django
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+# Import your models here
+
+# Create queries within functions
+from main_app.models import ZooDisplayAnimal
