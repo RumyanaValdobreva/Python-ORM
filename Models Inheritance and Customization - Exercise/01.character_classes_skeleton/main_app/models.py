f8bf6b785f16642e93451cbbc8c33d67708c47e7
Rumyana Valdobreva <rumyiwanowa@abv.bg>
2024-07-11 22:40:18 +0300

    Adding Models Inheritance and Customization - Exercise

---
+++ Models Inheritance and Customization - Exercise/01.character_classes_skeleton/main_app/models.py
@@ -0,0 +1,131 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+
+
+# Create your models here.
+
+class BaseCharacter(models.Model):
+    class Meta:
+        abstract = True
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Mage(BaseCharacter):
+    elemental_power = models.CharField(max_length=100)
+    spellbook_type = models.CharField(max_length=100)
+
+
+class Assassin(BaseCharacter):
+    weapon_type = models.CharField(max_length=100)
+    assassination_technique = models.CharField(max_length=100)
+
+
+class DemonHunter(BaseCharacter):
+    weapon_type = models.CharField(max_length=100)
+    demon_slaying_ability = models.CharField(max_length=100)
+
+
+class TimeMage(Mage):
+    time_magic_mastery = models.CharField(max_length=100)
+    temporal_shift_ability = models.CharField(max_length=100)
+
+
+class Necromancer(Mage):
+    raise_dead_ability = models.CharField(max_length=100)
+
+
+class ViperAssassin(Assassin):
+    venomous_strikes_mastery = models.CharField(max_length=100)
+    venomous_bite_ability = models.CharField(max_length=100)
+
+
+class ShadowbladeAssassin(Assassin):
+    shadowstep_ability = models.CharField(max_length=100)
+
+
+class VengeanceDemonHunter(DemonHunter):
+    vengeance_mastery = models.CharField(max_length=100)
+    retribution_ability = models.CharField(max_length=100)
+
+
+class FelbladeDemonHunter(DemonHunter):
+    felblade_ability = models.CharField(max_length=100)
+
+
+class UserProfile(models.Model):
+    username = models.CharField(max_length=70, unique=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(null=True, blank=True)
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(UserProfile, related_name="sent_messages", on_delete=models.CASCADE)
+    receiver = models.ForeignKey(UserProfile, related_name="received_messages", on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def mark_as_read(self):
+        self.is_read = True
+
+    def reply_to_message(self, reply_content: str):
+        message = Message(
+            sender=self.receiver,
+            receiver=self.sender,
+            content=reply_content,
+        )
+        message.save()
+        return message
+
+    def forward_message(self, receiver: UserProfile):
+        message = Message(
+            sender=self.receiver,
+            receiver=receiver,
+            content=self.content
+        )
+        message.save()
+        return message
+
+
+class StudentIDField(models.PositiveIntegerField):
+    def to_python(self, value):
+        try:
+            return int(value)
+        except ValueError:
+            raise ValueError("Invalid input for student ID")
+
+    def get_prep_value(self, value):
+        result = self.to_python(value)
+
+        if result <= 0:
+            raise ValidationError("ID cannot be less than or equal to zero")
+
+        return result
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    student_id = StudentIDField()
+
+
+class MaskedCreditCardField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 20
+        super().__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if not isinstance(value, str):
+            raise ValidationError("The card number must be a string")
+        if not value.isdigit():
+            raise ValidationError("The card number must contain only digits")
+        if len(value) != 16:
+            raise ValidationError("The card number must be exactly 16 characters long")
+
+        return f"****-****-****-{value[-4:]}"
+
+
+class CreditCard(models.Model):
+    card_owner = models.CharField(max_length=100)
+    card_number = MaskedCreditCardField()
