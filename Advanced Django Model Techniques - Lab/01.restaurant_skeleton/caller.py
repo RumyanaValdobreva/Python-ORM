06f2d763698fd95468102e95d719138a4a56ef2a
Rumyana Valdobreva <rumyiwanowa@abv.bg>
2024-07-16 00:21:45 +0300

    Adding Advanced Django Model Techniques - Lab

---
+++ Advanced Django Model Techniques - Lab/01.restaurant_skeleton/caller.py
@@ -0,0 +1,9 @@
+import os
+import django
+from django.core.exceptions import ValidationError
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+# Import your models here
---
+++ Advanced Django Model Techniques - Lab/01.restaurant_skeleton/main_app/validators.py
@@ -0,0 +1,6 @@
+from django.core.exceptions import ValidationError
+
+
+def validate_menu_categories(value):
+    if value not in ("Appetizers", "Main Course", "Desserts"):
+        raise ValidationError('The menu must include each of the categories "Appetizers", "Main Course", "Desserts".')
